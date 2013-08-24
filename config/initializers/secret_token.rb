c228987425cf2034c8e6f39a5aaa821a24ca34fd
Antonio Pardo <apardo@alabs.org>
2013-08-24 17:13:14 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myrtc::Application.config.secret_key_base = '19f3db9a229be9f3070de78335bf8beb9b2e99645c49276032e750ef08c10a3360c886982ccf4e25ab671773d3e61f75df30f2beac0cd6fefbd309dc99343c04'
