58e5e488000fc13d930783126149c37cd0301c45
Antonio Pardo <apardo@alabs.org>
2013-08-25 14:15:53 -0500

    prototipando

--- app/assets/javascripts/app/controllers/rooms_show_controller.js
+++ app/assets/javascripts/app/controllers/rooms_show_controller.js
@@ -7,4 +7,25 @@ angular.module('myRTC')
 		// Grab the room from the server
 		$scope.room = RoomFactory.get({id: $routeParams.name});
 
+    // create our webrtc connection
+    var webrtc = new SimpleWebRTC({
+        // the id/element dom element that will hold "our" video
+        localVideoEl: 'localVideo',
+        // the id/element dom element that will hold remote videos
+        remoteVideosEl: 'remotes',
+        // immediately ask for camera access
+        autoRequestMedia: true,
+        log: true
+    });
+  
+    var room = $routeParams.name;
+    console.log(room);
+    webrtc.createRoom(room);
+  
+    // when it's ready, join if we got a room from the URL
+    webrtc.on('readyToCall', function () {
+        // you can name it anything
+        webrtc.joinRoom(room);
+    });
+
 	}]);
