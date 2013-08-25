45c5e41d46f3dd5d5489d84406f1987bc8091f99
Antonio Pardo <apardo@alabs.org>
2013-08-25 16:58:11 -0500

    rtc ok

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,29 +30,28 @@
 
 $(function () {
 
-    // Iniciar el webrtc
-    $(".show-room").click(function() {
-      // create our webrtc connection
-      var webrtc = new SimpleWebRTC({
-          // the id/element dom element that will hold "our" video
-          localVideoEl: 'localVideo',
-          // the id/element dom element that will hold remote videos
-          remoteVideosEl: 'remotes',
-          // immediately ask for camera access
-          autoRequestMedia: true,
-          log: true
-      });
-  
-      // create the room in the provider
-      var room = $routeParams.name;
-      console.log('room');
-      webrtc.createRoom(room);
-  
-      // when it's ready, join 
-      webrtc.on('readyToCall', function () {
-          // you can name it anything
-          webrtc.joinRoom(room);
-      });
+  if ($("body#rtc-enabled").length) {
+    // create our webrtc connection
+    var webrtc = new SimpleWebRTC({
+        // the id/element dom element that will hold "our" video
+        localVideoEl: 'localVideo',
+        // the id/element dom element that will hold remote videos
+        remoteVideosEl: 'remotes',
+        // immediately ask for camera access
+        autoRequestMedia: true,
+        log: true
     });
-    
+  
+    // create the room in the provider
+    var room = window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
+    console.log(room);
+    webrtc.createRoom(room);
+  
+    // when it's ready, join 
+    webrtc.on('readyToCall', function () {
+        // you can name it anything
+        webrtc.joinRoom(room);
+    });
+  }
+
 });
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -10,6 +10,7 @@ class RoomsController < ApplicationController
   # GET /rooms/1
   # GET /rooms/1.json
   def show
+    @rtc = 'enabled' 
   end
 
   # GET /rooms/new
