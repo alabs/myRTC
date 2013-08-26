1a4701e9d699b1312aedbb00d23748e7c73166d4
Andrés Pereira <apereira@alabs.es>
2013-08-26 18:35:48 -0500

    frontend: ordenando javascripts y shareonfacebook

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,35 +27,9 @@
 // require app/controllers/contact_controller
 // require_tree .
 //= require rooms
+//= require facebook
+//= require twitter
+//= require webrtc
+//= require themeswitcher
 //= require_self
 
-$(function () {
-
-  if ($("body#rtc-enabled").length) {
-    // create our webrtc connection
-    var webrtc = new SimpleWebRTC({
-        // the id/element dom element that will hold "our" video
-        localVideoEl: 'localVideo',
-        // the id/element dom element that will hold remote videos
-        remoteVideosEl: 'remotes',
-        // immediately ask for camera access
-        autoRequestMedia: true,
-        log: true
-    });
-  
-    // create the room in the provider
-    var room = window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
-    webrtc.createRoom(room);
-    // when it's ready, join 
-    webrtc.on('readyToCall', function () {
-        // you can name it anything
-        webrtc.joinRoom(room);
-    });
-  }
-
-  $('#js-theme-switcher').on('change', function(){
-    $('#js-style-theme').remove();
-    $('head').append('<link id="js-style-theme" href="//netdna.bootstrapcdn.com/bootswatch/3.0.0/'+ this.value +'/bootstrap.min.css" rel="stylesheet">');
-  });
-
-});
