20a92edc60d98886036ff4729c558d9585a72585
Antonio Pardo <apardo@alabs.org>
2013-08-26 08:08:19 -0500

    fix js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -34,6 +34,12 @@
 
 $(function () {
 
+    $('#js-theme-switcher').on('change', function(){
+      $('#js-style-theme').remove();
+      $('head').append('<link id="js-style-theme" href="//netdna.bootstrapcdn.com/bootswatch/3.0.0/'+ this.value +'/bootstrap.min.css" rel="stylesheet">');
+    });
+
+
     // ejecutar en la página de la conferenncia
     if ($("body#rtc-enabled").length) {
   		var hash = window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
